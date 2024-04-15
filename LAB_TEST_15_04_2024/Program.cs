47231d82a26d5ef4f39c2a0d1e7defa9dcb3d265
AnatoliyKurilo <anatolijkurilo40@gmail.com>
2024-04-15 13:26:14 +0600

    Добавьте файлы проекта.

---
+++ LAB_TEST_15_04_2024/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_TEST_15_04_2024
+{
+    public class Program
+    {
+        public static void Pow_Test(int x, int n) {
+            if ((x >= 0 && x < 1000))
+            {
+
+                if ((n > 0 && n <= 100))
+                {
+                    Console.WriteLine("x в степени n = " + Math.Pow(x, n));
+                }
+                else {
+                    Console.WriteLine("число n за пределами диапозона [1;100]");
+                }
+            }
+            else {
+                Console.WriteLine("число x за пределами диапозона [0;999]");
+            }
+
+        }
+
+        public static void Test(string a, string b) {
+            //Console.WriteLine("Введите число");
+            int x;
+            int n;
+            if (int.TryParse(a, out x))
+            {
+                //Console.WriteLine("Введите степень");
+                if (int.TryParse(b, out n))
+                {
+                    Pow_Test(x, n);
+                }
+                else { Console.WriteLine("не число"); }
+            }
+            else
+            {
+                Console.WriteLine("не число");
+            }
+
+        }
+
+
+        public static void Main()
+        {
+            string a = "5";
+            string b = "2";
+            Test(a,b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03930ac5-d6ab-45e6-a522-f60bfdb3b17f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
